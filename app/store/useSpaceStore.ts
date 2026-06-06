97619c3c079f0e20a5237fb404e9d6a4cd6cdd88
ldbrian <ldbrian@163.com>
2026-06-06 17:48:27 +0800

    Update useSpaceStore.ts

--- app/store/useSpaceStore.ts
+++ app/store/useSpaceStore.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 
-export type Scene = 'entrance' | 'speaking' | 'trashing' | 'resting' | 'nostalgia' | 'browsing';
+export type Scene = 'entrance' | 'speaking' | 'trashing' | 'resting' | 'nostalgia' | 'roaming';
 
 interface SpaceState {
   currentScene: Scene;
